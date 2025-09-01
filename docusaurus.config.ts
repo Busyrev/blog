4ce37f3031720505c17b97953e14f7868b7f8779
Mike Busyrev <busyrev@gmail.com>
2025-09-01 20:50:53 +0300

    moved blog to busyrev.com

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://busyrev.github.io/',
+  url: 'https://busyrev.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/blog/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'busyrev', // Usually your GitHub org/user name.
-  projectName: 'blog', // Usually your repo name.
+  // organizationName: 'busyrev', // Usually your GitHub org/user name.
+  // projectName: 'blog', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
